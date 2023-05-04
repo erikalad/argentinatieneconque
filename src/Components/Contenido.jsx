c685e8da802d7c143bd35635495f06fc3613913c
Erika Ladner <erikaladner5@gmail.com>
2023-05-04 13:59:43 -0300

    nav

--- src/Components/Contenido.jsx
+++ src/Components/Contenido.jsx
@@ -7,6 +7,7 @@ import { LikeOutlined } from '@ant-design/icons';
 export default function Contenido() {
   return (
     <div className='contenedor-videos'>
+        <div className='contenedor-video'>
        <div>Es con las Pymes, el trabajo argentino y la industria nacional</div>
        <div className='video'>
        <iframe width="560" height="315" src="https://www.youtube.com/embed/BC7ShSKBpJQ?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -18,7 +19,67 @@ export default function Contenido() {
         <Statistic title="Me gusta" value={3} prefix={<LikeOutlined />} />
         </Col>
         </div>
-    
+        </div>
+
+        <div className='contenedor-video'>
+       <div>Felíz día a todas las y los trabajadores</div>
+       <div className='video'>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/Mhq3FIpH1g8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       
+       <Col >
+        <Statistic title="Visitas" value={566463} />
+        </Col>
+        <Col >
+        <Statistic title="Me gusta" value={7} prefix={<LikeOutlined />} />
+        </Col>
+        </div>
+        </div>
+
+
+        <div className='contenedor-video'>
+       <div>Es tiempo de Argentina</div>
+       <div className='video'>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/RHJfuum4vCk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       
+       <Col >
+        <Statistic title="Visitas" value={1405785} />
+        </Col>
+        <Col >
+        <Statistic title="Me gusta" value={4} prefix={<LikeOutlined />} />
+        </Col>
+        </div>
+        </div>
+
+
+        <div className='contenedor-video'>
+       <div>Más industrialización y autopartismo nacional</div>
+       <div className='video'>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/9mksAcRYwbo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       
+       <Col >
+        <Statistic title="Visitas" value={566455 } />
+        </Col>
+        <Col >
+        <Statistic title="Me gusta" value={10} prefix={<LikeOutlined />} />
+        </Col>
+        </div>
+        </div>
+
+        <div className='contenedor-video'>
+       <div>El motor productivo de la Argentina</div>
+       <div className='video'>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/SlGKAqdlGDo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       
+       <Col >
+        <Statistic title="Visitas" value={983947} />
+        </Col>
+        <Col >
+        <Statistic title="Me gusta" value={6} prefix={<LikeOutlined />} />
+        </Col>
+        </div>
+        </div>
+
+
     </div>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -6,20 +6,15 @@ import './Nav.css'
 export default function Nav() {
   return (
     <div>
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary nav">
                  <div class="container-fluid">
                 <img class="logo" src={logo}/>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 lista">
-                    <li class="nav-item">
-                    <a href='https://www.youtube.com/channel/UCqZfN80RcEttLEgPCle5G9w'><img class="logo-youtube" src={youtube}/></a>
-                    </li>
-                    
-                </ul>
-               
+                <div class="collapse navbar-collapse nav2" id="navbarTogglerDemo02" >
+                <a href='https://www.youtube.com/channel/UCqZfN80RcEttLEgPCle5G9w'><img class="logo-youtube" src={youtube}/></a>            
+                <div className='titulo'>Argentina tiene con que</div>
                 </div>
             </div>
             </nav>
